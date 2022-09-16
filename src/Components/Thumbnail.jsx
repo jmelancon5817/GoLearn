cafb45a9c98916cba3d49607d38416ec32bfc32d
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-09-15 19:43:46 -0500

    Add Thumbnail Component Styling

--- src/Components/Thumbnail.jsx
+++ src/Components/Thumbnail.jsx
@@ -1,7 +1,19 @@
-import SearchBar from "./SearchBar";
+import React from "react";
 
-const Thumbnail = () => {
-  return <div>Thumbnail</div>;
+const Thumbnail = (props) => {
+  return (
+    <div className=" border border-solid border-black-200 m-0 max-w-sm shadow-md rounded bg-gray-100 overflow-hidden mx-2 my-4">
+      <img
+        className="mx-auto h-2/3 rounded-t-lg w-2/3 mb-2 justify-center"
+        src={props.image}
+        alt=""
+      />
+      <div className="items-center">
+        <p className="font-bold text-l mb-2">{props.title}</p>
+        <p className="tracking-tight">by {props.author}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Thumbnail;
