eeeabe2213856d9dac52363df17ac83a3f40acf5
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-09-15 19:45:26 -0500

    Update Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav className="relative">
+      <div className="flex px-10 pt-20 sm:px-20 text-xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+        Genres - - -
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-slate-800 h-10 w-1/12" />
+    </nav>
+  );
 };
 
 export default Header;
