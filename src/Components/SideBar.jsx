47b8fc6a6ccf2b1375d50b12eba2a1ce067b2a0a
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-10-17 13:09:08 -0500

    Update SideBar Component

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,28 +1,57 @@
-import { useState } from "react";
-
 const SideBar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const sidebarData = [
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+    {
+      title: "Genre",
+      link: "/api",
+    },
+  ];
 
   return (
-    <>
-      {!isOpen ? (
-        <p>not open</p>
-      ) : (
-        <nav className="left-0 fixed bg-slate-500 h-full">
-          <div className="flex px-10 pt-20 sm:px-20 text-xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
-            <ul>
-              <li>Best Sellers</li>
-              <li>Trending</li>
-              <li>Literature</li>
-              <li>Science</li>
-              <li>History</li>
-              <li>Technology</li>
-              <li>Philosophy</li>
-            </ul>
-          </div>
-        </nav>
-      )}
-    </>
+    <div
+      className={`top-0 left-0 w-[15vw] mt-20 relative h-[100vw]  bg-slate-500 p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-200 
+      `}
+    >
+      <h3 className="flex justify-center mt-10 text-2xl font-semibold  text-gray-200">
+        <ul className="space-y-4 pr-8 hover:cursor-pointer ">
+          <li>Trending</li>
+          {sidebarData.map((val, key) => {
+            return (
+              <li key={key} className="hover:text-gray-400">
+                {val.title}
+              </li>
+            );
+          })}
+        </ul>
+      </h3>
+    </div>
   );
 };
 
