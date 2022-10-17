49c5ee6afa38ff4a58c7c70b6a1dfe1a485c5ae6
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-10-17 14:15:31 -0500

    Update Book Thumbnails

--- src/Components/Thumbnail.jsx
+++ src/Components/Thumbnail.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Thumbnail = (props) => {
   return (
-    <div className=" border border-solid border-black-200 m-0 max-w-sm shadow-md rounded bg-gray-100 overflow-hidden mx-2 my-4">
+    <div className=" border border-solid border-black-200 m-0 max-w-sm shadow-md rounded bg-zinc-50 overflow-hidden mx-2 my-4 h-80 w-52">
       <img
-        className="mx-auto h-2/3 rounded-t-lg w-2/3 mb-2 justify-center"
+        className="mx-auto h-4/6 rounded-t-lg w-10/12 mb-2 justify-center border"
         src={props.image}
         alt=""
       />
-      <div className="items-center">
-        <p className="font-bold text-l mb-2">{props.title}</p>
-        <p className="tracking-tight">by {props.author}</p>
+      <div className="items-center ml-4">
+        <p className="font-semibold text-l mb-1">{props.title}</p>
+        <p className="tracking-tight">by {props.author[0]}</p>
       </div>
     </div>
   );
